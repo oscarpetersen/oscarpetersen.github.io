85c6bfff5bc358dfcce49c2d360363c5732342a1
LAPTOP-55U75J5I\ozzy2 <ozzyp20025@gmail.com>
2018-04-11 14:10:35 +0200

    dokk os 1st test

---
+++ js/12.js
@@ -0,0 +1,9 @@
+var hackk document.getElementsByClassName('hack');
+var boddy document.getElementById('body');
+
+function hackScreen() {
+body.style.backgroundImage = 'url(hack.gif)';
+}
+
+hackk.onclick = 'hackScreen';
+p.innerHTML = 'whatup';
---
+++ js/script12.js
@@ -0,0 +1,8 @@
+var hackk document.getElementsByClassName('hack');
+var boddy document.getElementById('body');
+
+function hackScreen() {
+body.style.backgroundImage = 'url(hack.gif)';
+}
+
+hackk.onclick = 'hackScreen';
